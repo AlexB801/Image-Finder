5c20e23ca0ba51db28ef29a69b5859db340adc7d
Alex Bennett <benedictus.ab@gmail.com>
2018-05-11 16:21:01 -0600

    First Commit

---
+++ src/components/image-results/ImageResults.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { GridList, GridTile } from 'material-ui/GridList';
+import IconButton from 'material-ui/IconButton';
+
+
+class ImageResults extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+ImageResults.propTypes = {
+  images: PropTypes.array.isRequired
+}
+
+export default ImageResults;
